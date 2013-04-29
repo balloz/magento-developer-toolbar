466733556a9cca20ec05c34cbcd1b15ac05282b3
Bas Mostert <bas@iweb.co.uk>
2013-04-29 13:05:58 +0100

    Queries pane

--- app/code/local/Balloz/DeveloperToolbar/Block/Panel/Queries.php
+++ app/code/local/Balloz/DeveloperToolbar/Block/Panel/Queries.php
@@ -15,6 +15,18 @@ class Balloz_DeveloperToolbar_Block_Panel_Queries extends Balloz_DeveloperToolba
 		return 'Queries';
 	}
 
+	public function prettyInterval($interval) {
+		if ($interval < 10) {
+			$className = 'green';
+		} elseif ($interval < 20) {
+			$className = 'yellow';
+		} else {
+			$className = 'red';
+		}
+
+		return '<span class="query-interval '.$className.'">'.number_format($interval, 2).'ms</span>';
+	}
+
 	public function getQueries() {
 		$queries = array();
 		$resource = Mage::getSingleton('core/resource');
