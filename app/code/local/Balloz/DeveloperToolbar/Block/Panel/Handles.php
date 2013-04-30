38cd569bd4bb131c7dabc6d26d223a4dee81730a
Bas Mostert <bas@iweb.co.uk>
2013-04-30 13:06:45 +0100

    Added handles panel

---
+++ app/code/local/Balloz/DeveloperToolbar/Block/Panel/Handles.php
@@ -0,0 +1,16 @@
+<?php
+
+class Balloz_DeveloperToolbar_Block_Panel_Handles extends Balloz_DeveloperToolbar_Block_Panel
+{
+	public function getIdentifier() {
+		return 'handles';
+	}
+
+	public function getName() {
+		return 'Handles';
+	}
+
+	public function getLayoutHandles() {
+		return $this->getLayout()->getUpdate()->getHandles();
+	}
+}
