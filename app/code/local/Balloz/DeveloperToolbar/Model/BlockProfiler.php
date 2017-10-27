6cf5b9454fbe788bff0708d5d4ca9c9df1eeb41d
Bas Mostert <bas@iweb.co.uk>
2017-10-27 07:59:02 +0100

    Add "served from cache" status to block profiler

--- app/code/local/Balloz/DeveloperToolbar/Model/BlockProfiler.php
+++ app/code/local/Balloz/DeveloperToolbar/Model/BlockProfiler.php
@@ -6,10 +6,12 @@ class Balloz_DeveloperToolbar_Model_BlockProfiler
     protected $profiles = array();
     protected $stack = array();
     protected $helper;
+    protected $app;
 
     public function __construct()
     {
         $this->helper = Mage::helper('developertoolbar');
+        $this->app = Mage::app();
     }
 
     protected function getQueryCounts() {
@@ -59,6 +61,8 @@ class Balloz_DeveloperToolbar_Model_BlockProfiler
     }
 
     public function afterBlockRender(Varien_Event_Observer $event) {
+        $block = $event->getBlock();
+
         if (count($this->stack) === 0) {
             throw new Exception('Stack should not be empty!');
         }
@@ -67,12 +71,15 @@ class Balloz_DeveloperToolbar_Model_BlockProfiler
         $entry['query_end'] = $this->getQueryCounts();
         $entry['end'] = microtime(true);
 
+        $servedFromCache = (bool)$this->app->loadCache($block->getCacheKey());
+
         $this->profiles[$entry['profile_index']] = array(
             'level' => $entry['level'],
             'name' => $entry['name'],
             'class' => $entry['class'],
             'query_count' => $entry['query_end']['core_read'] - $entry['query_start']['core_write'],
-            'duration' => 1000 * ($entry['end'] - $entry['start'])
+            'duration' => 1000 * ($entry['end'] - $entry['start']),
+            'served_from_cache' => $servedFromCache
         );
     }
 
@@ -83,74 +90,4 @@ class Balloz_DeveloperToolbar_Model_BlockProfiler
     public function getProfiles() {
         return array_filter($this->profiles);
     }
-
-//
-//    public function onBlockCreation(Varien_Event_Observer $event)
-//    {
-//        $block = $event->getBlock();
-//        $name = $block->getNameInLayout();
-//
-//        if (!isset($this->blockQueries[$name])) {
-//            $this->blockQueries[$name] = array();
-//        }
-//
-//        $data = array();
-//        foreach ($this->helper->getQueries() as $connection => $queries) {
-//            $data[$connection] = array(
-//                'creation' => count($queries),
-//                'start' => false,
-//                'end' => false
-//            );
-//        }
-//
-//        $this->blockQueries[$name][spl_object_hash($block)] = array(
-//            'creation' => count($queries),
-//            'entries' => array()
-//        );
-//    }
-//
-//    public function beforeBlockRender(Varien_Event_Observer $event)
-//    {
-//        $block = $event->getBlock();
-//        $name = $block->getNameInLayout();
-//        $hash = spl_object_hash($block);
-//
-//        foreach ($this->helper->getQueries() as $connection => $queries) {
-//            if (isset($this->blockQueries[$name][$hash][$connection])) {
-//                $index = max(0, count($this->blockQueries[$name][$hash][$connection]) - 1);
-//            } else {
-//                $index = 0;
-//            }
-//
-//            echo 'START: ', get_class($block), "\n";
-//
-//            if (isset($this->blockQueries[$name][$hash][$connection][$index]['start'])) {
-//                var_dump($block); die();
-//                throw new Exception('This should not happen');
-//            }
-//
-//            $this->blockQueries[$name][$hash][$connection][$index] = array(
-//                'start' => count($queries)
-//            );
-//        }
-//    }
-//
-//    public function afterBlockRender(Varien_Event_Observer $event)
-//    {
-//        $block = $event->getBlock();
-//        $name = $block->getNameInLayout();
-//        $hash = spl_object_hash($block);
-//
-//        foreach ($this->helper->getQueries() as $connection => $queries) {
-//            $index = max(0, count($this->blockQueries[$name][$hash][$connection]) - 1);
-//
-//            echo 'END: ', get_class($block), "\n";
-//
-//            if (isset($this->blockQueries[$name][$hash][$connection][$index]['end'])) {
-//                throw new Exception('This should not happen');
-//            }
-//
-//            $this->blockQueries[$name][$hash][$connection][$index]['end'] = count($queries);
-//        }
-//    }
 }
