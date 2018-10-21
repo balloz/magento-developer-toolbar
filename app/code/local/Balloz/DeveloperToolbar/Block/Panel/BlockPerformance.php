27a15064106f5a9e1a6cabff738ebf0aeeccb18a
Bas Mostert <bas@iweb.co.uk>
2018-10-21 09:57:08 +0100

    Add percentages to block performance panel

--- app/code/local/Balloz/DeveloperToolbar/Block/Panel/BlockPerformance.php
+++ app/code/local/Balloz/DeveloperToolbar/Block/Panel/BlockPerformance.php
@@ -2,6 +2,8 @@
 
 class Balloz_DeveloperToolbar_Block_Panel_BlockPerformance extends Balloz_DeveloperToolbar_Block_Panel
 {
+    protected $total;
+
     public function getIdentifier()
     {
         return 'block-performance';
@@ -15,6 +17,21 @@ class Balloz_DeveloperToolbar_Block_Panel_BlockPerformance extends Balloz_Develo
     public function getProfiles()
     {
         return Mage::getSingleton('developertoolbar/blockProfiler')->getProfiles();
+    }
+
+    public function getPercentage($source)
+    {
+        if ($this->total === null) {
+            $total = 0;
+
+            foreach ($this->getProfiles() as $profile) {
+                $total += $profile['duration'];
+            }
+
+            $this->total = $total;
+        }
+
+        return round(100 * $source['duration'] / $this->total, 2);
     }
 
     public function colorInterval($interval)
